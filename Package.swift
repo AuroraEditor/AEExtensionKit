f231cc7303678c217c0fe34f9b3be3289fc576ce
Wesley De Groot <email@wesleydegroot.nl>
2022-08-18 14:04:49 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@
 import PackageDescription
 
 let package = Package(
-    name: "CodeEditKit",
+    name: "AEExtensionKit",
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
-            name: "CodeEditKit",
+            name: "AEExtensionKit",
             type: .dynamic,
-            targets: ["CodeEditKit"]),
+            targets: ["AEExtensionKit"]),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,10 +20,10 @@ let package = Package(
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
-            name: "CodeEditKit",
+            name: "AEExtensionKit",
             dependencies: []),
         .testTarget(
-            name: "CodeEditKitTests",
-            dependencies: ["CodeEditKit"]),
+            name: "AEExtensionKitTests",
+            dependencies: ["AEExtensionKit"]),
     ]
 )
