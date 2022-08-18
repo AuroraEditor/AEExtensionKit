b56c5afc272054187bc80d173d5a35eda6564677
Wesley de Groot <email@wesleydegroot.nl>
2022-08-18 14:07:34 +0200

    Updates

--- Sources/CodeEditKit/Targets/Target.swift
+++ Sources/CodeEditKit/Targets/Target.swift
@@ -10,15 +10,15 @@ import Foundation
 /// This structure stores information about the target to be available in CodeEdit for running
 public struct Target: Identifiable {
 
-    /**
-     * Initializes a target with parameters
-     * - Parameter id: The unique identifier of the target set by the extension
-     * - Parameter displayName: The name of the target to be displayed in the UI
-     * - Parameter executable: The executable to launch inside the pseudo terminal
-     * - Parameter args: an array of strings that is passed as the arguments to the underlying process
-     * - Parameter environment: an array of environment variables to pass to the child process.
-     * - Parameter execName: If provided, this is used as the Unix argv[0] parameter, otherwise, the executable is used as the args [0], this is used when the intent is to set a different process name than the file that backs it.
-     */
+    /// Initializes a target with parameters
+    ///  - Parameter id: The unique identifier of the target set by the extension
+    ///  - Parameter displayName: The name of the target to be displayed in the UI
+    ///  - Parameter executable: The executable to launch inside the pseudo terminal
+    ///  - Parameter args: an array of strings that is passed as the arguments to the underlying process
+    ///  - Parameter environment: an array of environment variables to pass to the child process.
+    ///  - Parameter execName: If provided, this is used as the Unix argv[0] parameter, otherwise, 
+    /// the executable is used as the args [0], this is used when the intent is to set a different
+    /// process name than the file that backs it.
     public init(id: String, displayName: String,
                 executable: String, args: [String] = [],
                 environment: [String]? = nil, execName: String? = nil) {
--- Tests/CodeEditKitTests/AEExtensionKitTests.swift
+++ Tests/CodeEditKitTests/AEExtensionKitTests.swift
@@ -1,7 +1,7 @@
 import XCTest
 @testable import CodeEditKit
 
-final class CEExtensionKitTests: XCTestCase {
+final class AEExtensionKitTests: XCTestCase {
     func testExample() throws {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
